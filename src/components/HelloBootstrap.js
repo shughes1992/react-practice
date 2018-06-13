8bb742deb9fb2788f7324f0028239828dc315062
Samantha Hughes <hughes2@law.upenn.edu>
2018-06-12 20:35:17 -0400

    practicing bootsrap with React

---
+++ src/components/HelloBootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HelloBootstrap = () => (
+
+
+<div className="container">
+    <div className="jumbotron">
+      <h1>Hello, world!</h1>
+      <p>I'm a jumbotron!</p>
+
+    </div>
+</div>
+
+
+);
+export default HelloBootstrap;
+
